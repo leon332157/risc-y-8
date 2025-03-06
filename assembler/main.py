2e310305770185ae605011cf9d78d8ff6c6d5217
leon332157 <leon332157@gmail.com>
2025-03-06 02:18:26 -0500

    i promise this grammar is correct

--- assembler/main.py
+++ assembler/main.py
@@ -17,60 +17,161 @@ from pyleri import end_of_statement
 import pprint
 from re import IGNORECASE
 
-WORDSIZE = 4 # bytes
+WORDSIZE = 4  # bytes
+
 
 class ASMGrammar(Grammar):
-
     NEW_LINE = Regex(r"(\r\n|\r|\n)")
     COMMA = ","
-    # LABEL = Regex(r'([a-zA-Z_][a-zA-Z0-9_]*):')
-    # END = Regex(r'')
-    INST = Regex(r"[a-z]{3}", IGNORECASE)
-    REG_DEST = Regex(r"R[0-9]+", IGNORECASE)
-    REG_SRC = Regex(r"R[0-9]+", IGNORECASE)
-    IMM_HEX = Regex(r"^(0x)?[0-9a-f]+", IGNORECASE)
-    IMM_DEC = Regex(r"^-?[0-9]+")
-    IMM = Choice(IMM_HEX, IMM_DEC, most_greedy=False)
 
-    DIRECTIVES = Sequence(Regex(r".[a-zA-Z]+", IGNORECASE), Optional(IMM))
-    ORIGIN = Sequence(Regex(r".org", IGNORECASE), Optional(IMM))
-
-    INSTR_REG_REG = Sequence(INST, REG_DEST, COMMA, REG_SRC)
-    INSTR_IMM_REG = Sequence(INST, REG_DEST, COMMA, IMM)
-    INSTR_CTRL = Sequence(INST, Optional(Choice(IMM, REG_DEST, most_greedy=False)))
-    INSTR_MEM = Sequence(
-        INST, REG_DEST, Choice(IMM, REG_SRC, most_greedy=False), Optional(IMM)
+    """
+    Base set GP registers: r0 (zero), r1-r16, bp, sp, pc
+    """
+    REG_ZERO = Regex(r"^r0", IGNORECASE)
+    "0x00"
+    REG_GP = Regex(r"^r[1-9][0-6]?", IGNORECASE)
+    "0x01 - 0x10 respectively"
+    REG_BP = Regex(r"^bp|^rbp", IGNORECASE)
+    "0x0E"
+    REG_SP = Regex(r"sp|rsp", IGNORECASE)
+    "0x0F"
+    REG_LR = Regex(r"lr", IGNORECASE)
+    "0x10"
+    REG_INT_FLAG = Regex(r"rflag", IGNORECASE)
+    "0x11"
+    REG_PC = Regex(r"pc", IGNORECASE)
+    "0x1F"
+    REG_ALL_GP = Choice(
+        REG_ZERO, REG_GP, REG_BP, REG_SP, REG_LR, REG_PC, most_greedy=False
     )
-    INSTR_MEM_BRACKET = Sequence(
-        INST, REG_DEST, COMMA, Sequence("[", REG_SRC, Tokens("+ - *"), IMM, "]")
+    "all registers in base set"
+
+    REG_FP = Regex(r"f[1-8]", IGNORECASE)
+    "FP registers: f1-f8"
+
+    REG_VEC = Regex(r"v[1-8]", IGNORECASE)
+    "Vector registers: v1-v8"
+    REG_VFLAG = Regex(r"vflag", IGNORECASE)  # 0x1D
+    REG_VTYPE = Regex(r"vtype", IGNORECASE)  # 0x1E
+
+    REG_ALL = Choice(
+        REG_ALL_GP,
+        REG_FP,
+        REG_VEC,
+        REG_INT_FLAG,
+        REG_VFLAG,
+        REG_VTYPE,
+        most_greedy=False,
+    )
+    "all registers"
+
+    """
+    Immediate notation: 0x1234 (hex), 1234 (decimal)
+    """
+    IMM_HEX = Regex(r"^0x?[0-9a-f]+", IGNORECASE)  # hex
+    IMM_DEC = Regex(r"^-?[0-9]+")  # decimal
+    IMM = Choice(
+        IMM_HEX, IMM_DEC, most_greedy=False
+    )  # Immediate notation: 0x1234 or 1234 (hex or decimal)
+
+    """
+    Memory notation: [0x100],[r1], [r1+0x10]
+    """
+    MEM_IMM = Sequence("[", IMM, "]")
+    MEM_REG = Sequence("[", REG_ALL_GP, "]")
+    MEM_REG_DISP = Sequence(Sequence("[", REG_ALL_GP, Tokens("+ -"), IMM, "]"))
+    MEM = Choice(MEM_IMM, MEM_REG, MEM_REG_DISP, most_greedy=False)
+    "all memory notations"
+
+    """
+    Instructions
+    """
+    INST = Regex(r"^[a-z]+", IGNORECASE)
+    INST_ONLY = Sequence(INST)
+    INST_REG = Sequence(INST, REG_ALL)
+    INST_MEM = Sequence(INST, MEM)
+    INST_IMM = Sequence(INST, IMM)
+    INST_REG_IMM = Sequence(INST, REG_ALL_GP, COMMA, IMM)
+    INST_REG_REG = Sequence(INST, REG_ALL_GP, COMMA, REG_ALL_GP)
+    INST_REG_MEM = Sequence(INST, REG_ALL_GP, COMMA, MEM)
+    INST_BASE = Choice(
+        INST_ONLY,
+        INST_REG,
+        INST_MEM,
+        INST_IMM,
+        INST_REG_IMM,
+        INST_REG_REG,
+        INST_REG_MEM,
+        most_greedy=True
     )
 
+    INST_FR = Sequence(INST, REG_FP)
+    INST_FR_FR = Sequence(INST, REG_FP, COMMA, REG_FP)
+    INST_FR_FR_FR = Sequence(INST, REG_FP, COMMA, REG_FP, COMMA, REG_FP)
+    INST_FR_MEM = Sequence(INST, REG_FP, COMMA, MEM)
+    INST_FR_FR_MEM = Sequence(INST, REG_FP, COMMA, REG_FP, COMMA, MEM)
+    INST_FLOAT = Choice(
+        INST_FR,
+        INST_FR_FR,
+        INST_FR_FR_FR,
+        INST_FR_MEM,
+        INST_FR_FR_MEM,
+        most_greedy=True
+    )
+    
+    VEC_TYPE = Regex(r"i|f|d|b", IGNORECASE)
+    INST_VREG = Sequence(INST, REG_VEC)
+    INST_VT_VREG_MEM =  Sequence(INST,'.',VEC_TYPE, REG_VEC, COMMA, COMMA, MEM)
+    INST_VREG_VREG_VREG = Sequence(INST, REG_VEC, COMMA, REG_VEC, COMMA, REG_VEC)
+    INST_VREG_VREG_IMM = Sequence(INST, REG_VEC, COMMA, REG_VEC, COMMA, IMM)
+    INST_VEC = Choice(
+        INST_VREG,
+        INST_VT_VREG_MEM,
+        INST_VREG_VREG_VREG,
+        INST_VREG_VREG_IMM,
+        most_greedy=True
+    )
+    
     INSTRUCTION = Choice(
-        INSTR_REG_REG, INSTR_IMM_REG, INSTR_CTRL, INSTR_MEM, INSTR_MEM_BRACKET
+        INST_BASE,
+        INST_FLOAT,
+        INST_VEC,
+        most_greedy=True,
     )
+    LABEL = Regex(r"\.[a-zA-Z]+")
+    DIRECTIVES = Sequence(LABEL, Optional(IMM))
+    # ORIGIN = Sequence(Regex(r".org", IGNORECASE), Optional(IMM))
+    CODE_LABEL = Sequence(LABEL,":")
     # LINES = Ref()
-    LINE = Sequence(
-        Choice(DIRECTIVES, INSTRUCTION, most_greedy=False), Optional(NEW_LINE)
-    )
-    LINES = Repeat(LINE)
-    START = Sequence(Optional(ORIGIN), LINES)
+    # LINE_DIRECTIVE = Sequence(DIRECTIVES,NEW_LINE)
+    # LINE_LABEL = Sequence(LABEL, NEW_LINE)
+    LINE_COMMENT = Regex(r"^#.*")
+    LINE = Choice(LINE_COMMENT, INSTRUCTION, most_greedy=False)
+
+    START = Repeat(LINE)  # Optional(ORIGIN), LINES)
+
 
 class Operand:
-    def __init__(self, value, size = WORDSIZE):
+    def __init__(self, value, size=WORDSIZE):
         self.size = size
         if not isinstance(value, int):
             raise TypeError("Value of operand must be an integer")
-        if value > 2**(size*8):
+        if value > 2 ** (size * 8):
             raise ValueError("Value of operand is too large")
         self.value = value
-        
+
+
 class InstructionEncoding:
-    def __init__(self,inst:Operand, op1:Operand, op2:Operand):
+    def __init__(self, inst: Operand, op1: Operand, op2: Operand):
         self.inst = inst
-    def encode(self)->bytes[4]:
+
+    def encode(self) -> bytearray:
         pass
 
+
 asm_grammar = ASMGrammar()
+
+
 def print_expecting(node_expecting, string_expecting):
     for loop, e in enumerate(node_expecting):
         string_expecting = "{}\n\t({}) {}".format(string_expecting, loop, e)
@@ -90,8 +191,8 @@ def auto_correction(string, my_grammar):
         string_expecting = (
             "String is NOT valid.\nExpected: "
             if not node.pos
-            else 'String is NOT valid. \nAfter "{}" expected: '.format(
-                node.tree.string[: node.pos]
+            else 'String is NOT valid. \nAfter "{}" {} expected: '.format(
+                node.tree.string[: node.pos],node.pos
             )
         )
         print_expecting(node.expecting, string_expecting)
@@ -122,13 +223,38 @@ def view_parse_tree(res):
 
 
 TEST = """
-.ORG 0x1000
+#.ORG 0x1000
+#xor r1, r1
 add r1, 1
+xor r2, r2
+add r2, 2
+add r1, r2
+cmp r2, 0x10
+nop
+xor r3,r3
+orr r3, 0x3f80
+shl r3, 16
+push r3
+fldr f1, [sp]
+pop r3
+xor r3, r3
+add r3, 0x4
+shl r3, 28
+push r3
+fldr f2, [sp]
+fadd f3, f1, f2
+fmul f4, f3, f2
+fsqrt f5, f4
+bunc 0x1000
+hlt
+VZEROALL v0
+VZEROUPPER v1
+VBEQ v2, v3, -10
 """
 
 gmr = asm_grammar.parse(TEST)
 
-# print(asm_grammar.export_go())
+print(asm_grammar.export_go())
 pp = pprint.PrettyPrinter(indent=1, sort_dicts=True, compact=True)
 pp.pprint(view_parse_tree(gmr))
 
