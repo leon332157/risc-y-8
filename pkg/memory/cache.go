290207a4bdd670395beea5f1d16e6da2223ce526
megraves <megraves@umass.edu>
2025-03-11 15:49:41 -0400

    move cache functions from ram.go file to cache.go

--- pkg/memory/cache.go
+++ pkg/memory/cache.go
@@ -2,6 +2,7 @@ package memory
 
 import (
 	"container/list" // for LRU queue
+	"fmt"
 )
 
 type Cache interface {
@@ -29,4 +30,128 @@ type CacheType struct {
 	access   AccessState
 	sets     []Set
 	memory   RAM
+}
+
+// CACHE FUNCTIONS:
+
+// Creates the default cache
+func createDefault(mem RAM) CacheType {
+	return configureCache(4, 4, 2, 0, mem)
+}
+
+// Creates a cache with configurable params
+func configureCache(lineSize, numSets, ways, latency int, mem RAM) CacheType {
+
+	access := createAccessState(latency)
+	// initialize the sets
+	sets := make([]Set, numSets)
+
+	// Initialize empty sets with lru queue
+	for i := range sets {
+		sets[i] = Set{
+			lines:    make([]CacheLine, ways),
+			LRUQueue: list.New(),
+		}
+		// Initialize sets with empty lines
+		for j := range sets[i].lines {
+			sets[i].lines[j] = CacheLine{data: make([]uint32, lineSize), valid: false, dirty: false}
+		}
+	}
+
+	// Return the new cache (return pointer instead??)
+	return CacheType{
+		lineSize: lineSize,
+		numSets:  numSets,
+		ways:     ways,
+		access:   *access,
+		sets:     sets,
+		memory:   mem,
+	}
+}
+
+// Checks cache access and depending on hit or miss, updates cache based on results
+func (c *CacheType) search(addr int) bool {
+
+	// If c can't be accessed, return false
+	if !c.access.AccessAttempt() {
+		return false
+	}
+
+	// Get the index, tag, from the address
+	index := (addr / c.lineSize) % c.numSets
+	tag := addr / (c.lineSize * c.numSets)
+	set := &c.sets[index]
+
+	// for each line in the set
+	for i, line := range set.lines {
+		// if its valid and the tag matches
+		if line.valid && line.tag == tag {
+			c.updateLRU(set, i)
+			return true // Cache hit
+		}
+		if !line.valid {
+			// TODO
+		}
+	}
+	// Cache miss
+	// TODO:
+	// add data to first invalid line from memory
+	//c.sets[index].lines[mn] =
+	// OR if full, evict and replace (write back)
+	c.evictAndReplace(set, tag, addr)
+	return true
+}
+
+// Update the LRU queue to see which line must be evicted next
+func (c *CacheType) updateLRU(set *Set, idx int) {
+
+	for e := set.LRUQueue.Front(); e != nil; e = e.Next() {
+		if e.Value.(int) == idx {
+			set.LRUQueue.Remove(e)
+			break
+		}
+	}
+	set.LRUQueue.PushFront(idx)
+}
+
+func (c *CacheType) evictAndReplace(set *Set, tag int, addr int) {
+
+	victimIdx := c.getLRUVictim(set)
+	set.lines[victimIdx].tag = tag
+	set.lines[victimIdx].valid = true
+	c.updateLRU(set, victimIdx)
+
+	// TODO:
+	// Read desired data from memory
+	//toLoad := c.memory.read(addr, true)
+
+	// delay?
+	for range c.memory.access.latency {
+		c.memory.read(addr, false)
+	}
+	// write evicted data to memory
+	//c.memory.write(addr, )
+
+	//delay
+
+	// write new data in cache
+}
+
+func (c *CacheType) getLRUVictim(set *Set) int {
+
+	elem := set.LRUQueue.Back()
+	if elem != nil {
+		return elem.Value.(int)
+	}
+	return 0
+}
+
+func PrintCache(cache CacheType) {
+
+	for i := 0; i < len(cache.sets); i++ {
+		for j := 0; j < len(cache.sets[i].lines); j++ {
+			fmt.Println(cache.sets[i].lines[j].data)
+		}
+	}
+	fmt.Println("DONE")
 }
--- pkg/memory/ram.go
+++ pkg/memory/ram.go
@@ -1,7 +1,6 @@
 package memory
 
 import (
-	"container/list" // for LRU queue
 	"fmt"
 )
 
@@ -46,7 +45,7 @@ func createAccessState(latency int) *AccessState {
 }
 
 // Returns a bool to check if the mem has been accessed during the cycle
-func (c *AccessState) accessAttempt() bool {
+func (c *AccessState) AccessAttempt() bool {
 
 	// If mem has been accessed, decrement cycles left and return false (must wait!)
 	if c.accessed && c.cyclesLeft != 0 {
@@ -103,7 +102,7 @@ func (mem *RAM) addrToOffset(addr int) (int, int) {
 func (mem *RAM) read(addr int, lin bool) *RAMValue {
 
 	// if memory cannot be accessed, it returns nothing (read more here about nil https://go101.org/article/nil.html)
-	if !mem.access.accessAttempt() {
+	if !mem.access.AccessAttempt() {
 		return nil
 	}
 
@@ -125,7 +124,7 @@ func (mem *RAM) read(addr int, lin bool) *RAMValue {
 func (mem *RAM) write(addr int, val *RAMValue) bool {
 
 	// memory cannot be accessed, return false
-	if !mem.access.accessAttempt() {
+	if !mem.access.AccessAttempt() {
 		return false
 	}
 
@@ -157,122 +156,7 @@ func (mem *RAM) flash(instructions []uint32) {
 	}
 }
 
-// CACHE FUNCTIONS:
-
-// Creates the default cache
-func createDefault(mem RAM) CacheType {
-	return configureCache(4, 4, 2, 0, mem)
-}
-
-// Creates a cache with configurable params
-func configureCache(lineSize, numSets, ways, latency int, mem RAM) CacheType {
-
-	access := createAccessState(latency)
-	// initialize the sets
-	sets := make([]Set, numSets)
-
-	// Initialize empty sets with lru queue
-	for i := range sets {
-		sets[i] = Set{
-			lines:    make([]CacheLine, ways),
-			LRUQueue: list.New(),
-		}
-		// Initialize sets with empty lines
-		for j := range sets[i].lines {
-			sets[i].lines[j] = CacheLine{data: make([]uint32, lineSize), valid: false, dirty: false}
-		}
-	}
-
-	// Return the new cache (return pointer instead??)
-	return CacheType{
-		lineSize: lineSize,
-		numSets:  numSets,
-		ways:     ways,
-		access:   *access,
-		sets:     sets,
-		memory:   mem,
-	}
-}
-
-// Checks cache access and depending on hit or miss, updates cache based on results
-func (c *CacheType) search(addr int) bool {
-
-	// If c can't be accessed, return falses
-	if !c.access.accessAttempt() {
-		return false
-	}
-
-	// Get the index, tag, from the address
-	index := (addr / c.lineSize) % c.numSets
-	tag := addr / (c.lineSize * c.numSets)
-	set := &c.sets[index]
-
-	// for each line in the set
-	for i, line := range set.lines {
-		// if its valid and the tag matches
-		if line.valid && line.tag == tag {
-			c.updateLRU(set, i)
-			return true // Cache hit
-		}
-		if !line.valid {
-			// TODO
-		}
-	}
-	// Cache miss
-	// TODO:
-	// add data to first invalid line from memory
-	//c.sets[index].lines[mn] =
-	// OR if full, evict and replace (writeback)
-	c.evictAndReplace(set, tag, addr)
-	return true
-}
-
-// Update the LRU queue to see which line must be evicted next
-func (c *CacheType) updateLRU(set *Set, idx int) {
-
-	for e := set.LRUQueue.Front(); e != nil; e = e.Next() {
-		if e.Value.(int) == idx {
-			set.LRUQueue.Remove(e)
-			break
-		}
-	}
-	set.LRUQueue.PushFront(idx)
-}
-
-func (c *CacheType) evictAndReplace(set *Set, tag int, addr int) {
-
-	victimIdx := c.getLRUVictim(set)
-	set.lines[victimIdx].tag = tag
-	set.lines[victimIdx].valid = true
-	c.updateLRU(set, victimIdx)
-
-	// TODO:
-	// Read desired data from memory
-	//toLoad := c.memory.read(addr, true)
-
-	// delay?
-	for range c.memory.access.latency {
-		c.memory.read(addr, false)
-	}
-	// write evicted data to memory
-	//c.memory.write(addr, )
-
-	//delay
-
-	// write new data in cache
-}
-
-func (c *CacheType) getLRUVictim(set *Set) int {
-
-	elem := set.LRUQueue.Back()
-	if elem != nil {
-		return elem.Value.(int)
-	}
-	return 0
-}
-
 func main() {
-	// TODO:
 	mem := createRAM(256, 4, 32, 3)
 	mem.write(10, &RAMValue{value: 123})
 	for i := 0; i < 10; i += 1 {
@@ -287,21 +171,11 @@ func main() {
 	}
 
 	cache := createDefault(mem)
-	print(cache)
+	PrintCache(cache)
 
 	cache.search(10)
-	print(cache)
+	PrintCache(cache)
 
 	cache.search(32)
-	print(cache)
-}
-
-func print(cache CacheType) {
-
-	for i := 0; i < len(cache.sets); i++ {
-		for j := 0; j < len(cache.sets[i].lines); j++ {
-			fmt.Println(cache.sets[i].lines[j].data)
-		}
-	}
-	fmt.Println("DONE")
+	PrintCache(cache)
 }
