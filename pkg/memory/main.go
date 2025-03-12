0777b19d5247de95d937f58411070803d8884703
megraves <megraves@umass.edu>
2025-03-11 21:27:39 -0400

    comment out tests so it doesn't get in richard's way

--- pkg/memory/main.go
+++ pkg/memory/main.go
@@ -1,31 +1,31 @@
 package memory
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func main() {
-	var mem Memory
-	ram := mem.CreateRAM(256, 4, 32, 3)
-	ram.Write(10, &RAMValue{value: 123})
-	for i := 0; i < 10; i += 1 {
+// func main() {
+// 	var mem Memory
+// 	ram := mem.CreateRAM(256, 4, 32, 3)
+// 	ram.Write(10, &RAMValue{value: 123})
+// 	for i := 0; i < 10; i += 1 {
 
-		val := mem.Read(10, false)
+// 		val := mem.Read(10, false)
 
-		if val == nil {
-			fmt.Println("Read: ", "WAIT")
-		} else {
-			fmt.Println("Read: ", val.value) // should print "Read: 123"
-		}
-	}
+// 		if val == nil {
+// 			fmt.Println("Read: ", "WAIT")
+// 		} else {
+// 			fmt.Println("Read: ", val.value) // should print "Read: 123"
+// 		}
+// 	}
 
-	var cache Cache
-	c := cache.CreateDefault(ram)
-	PrintCache(c)
+// 	var cache Cache
+// 	c := cache.CreateDefault(ram)
+// 	PrintCache(c)
 
-	c.Search(10)
-	PrintCache(c)
+// 	c.Search(10)
+// 	PrintCache(c)
 
-	c.Search(32)
-	PrintCache(c)
-}
+// 	c.Search(32)
+// 	PrintCache(c)
+// }
