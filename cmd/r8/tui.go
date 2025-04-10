7a46599ab40821bab7e7e10c2a2033f07858eb7a
megraves <megraves@umass.edu>
2025-04-09 21:03:01 -0400

    cache and ram tables

--- cmd/r8/tui.go
+++ cmd/r8/tui.go
@@ -1,10 +1,20 @@
 package r8
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/charmbracelet/bubbles/table"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/leon332157/risc-y-8/pkg/memory"
 )
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240")).
+	BorderBackground(lipgloss.Color("240"))
 
 // TODO: develop models to represents necessary components of our TUI
 // RAM & Cache --> (configurable cache ??)
@@ -12,7 +22,21 @@ import (
 // Pipeline Stages
 // Current instruction
 type Model struct {
+	// embed other models on page
+	ram       table.Model
+	cache     table.Model
+	regs      table.Model
+	instr     textinput.Model
+	fetch     table.Model //single column table
+	decode    table.Model
+	execute   table.Model
+	memory    table.Model
+	writeback table.Model
 }
+
+// type RamModel struct {
+// 	ram table.Model
+// }
 
 func (m Model) Init() tea.Cmd {
 	return nil
@@ -27,16 +51,85 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c": // key to exit the tui
 			return m, tea.Quit
 		}
-		// TODO: Click events
+		// TODO: Click events??
+		// forward arrow advances the pipeline by one stage
+		// ctrl+n advances the pipeline by 5 stages?? (1 cycle)
 	}
 	return m, nil
 }
 
 func (m Model) View() string {
-	return "have a cup of tea!"
+	return baseStyle.Render(m.cache.View()) //+ baseStyle.Render(m.ram.View()) + "\n"
 }
 
 func TUIMain() {
+
+	// RAM DISPLAY:
+	ram := memory.CreateRAM(32, 8, 5)
+
+	// loop thru contents, make RAM rows
+	rRows := []table.Row{}
+	addr := 0
+	for range ram.NumLines {
+		row := []string{}
+		for range ram.WordsPerLine {
+			row = append(row, fmt.Sprintf("%08X", ram.Contents[addr]))
+			addr++
+		}
+		rRows = append(rRows, row)
+	}
+
+	rm := table.New(
+		table.WithColumns([]table.Column{
+			{Title: "RAM", Width: 8}, {Width: 8}, {Width: 8}, {Width: 8},
+			{Width: 8}, {Width: 8}, {Width: 8}, {Width: 8},
+		}),
+		table.WithRows(rRows),
+		table.WithFocused(true),
+		table.WithHeight(int(ram.NumLines)+1),
+	)
+
+	// CACHE DISPLAY:
+	cache := memory.CreateCacheDefault(&ram)
+
+	cRows := []table.Row{}
+	for i := range cache.Contents {
+		for j := range cache.Ways {
+			data := cache.Contents[i][j]
+			row := []string{
+				fmt.Sprintf("%05b", data.Tag),
+				fmt.Sprintf("%03b", i),
+				fmt.Sprintf("%08X", data.Data),
+				fmt.Sprintf("%t", data.Valid),
+				fmt.Sprintf("%d", data.LRU)}
+			cRows = append(cRows, row)
+		}
+	}
+
+	c := table.New(
+		table.WithColumns([]table.Column{
+			{Title: "Tag", Width: 5}, {Title: "Index", Width: 5}, {Title: "Data", Width: 8},
+			{Title: "Valid", Width: 5}, {Title: "LRU", Width: 3},
+		}),
+		table.WithRows(cRows),
+		table.WithFocused(true),
+		table.WithHeight(int(cache.Sets*cache.Ways)+1),
+	)
+
+	rg := table.New(
+	// How are registers created?
+	)
+
+	i := textinput.New()
+
+	fet := table.New()
+	dec := table.New()
+	exe := table.New()
+	mem := table.New()
+	wb := table.New()
+
+	m := Model{rm, c, rg, i, fet, dec, exe, mem, wb}
+
 	// Log details to a file for debugging purposes
 	f, err := tea.LogToFile("debug.log", "debug")
 	if err != nil {
@@ -45,9 +138,8 @@ func TUIMain() {
 	defer f.Close()
 
 	// Run a new tea program
-	program := tea.NewProgram(Model{}, tea.WithAltScreen())
+	program := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := program.Run(); err != nil {
 		log.Fatal(err)
 	}
-
 }
